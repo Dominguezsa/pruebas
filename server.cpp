e731cc13c1913b278a8f0b6a2921d798e990f67e
Dominguezsa <sadominguez@fi.uba.ar>
2025-01-30 12:30:05 +0000

    cambio read por recv

--- server.cpp
+++ server.cpp
@@ -19,7 +19,7 @@ void manejar_cliente(int cliente_fd) {
     std::string mensajeAcumulado;
 
     while (true) {
-        ssize_t bytes_leidos = read(cliente_fd, buffer, 1);
+        ssize_t bytes_leidos = recv(cliente_fd, buffer, sizeof(buffer), 0);
         if (bytes_leidos < 0) {
             error("Error al leer del socket");
         }
@@ -37,7 +37,9 @@ void manejar_cliente(int cliente_fd) {
 
             // Enviar la longitud de la palabra
             std::string respuesta = std::to_string(mensajeAcumulado.length()) + " ";
-            ssize_t bytes_enviados = write(cliente_fd, respuesta.c_str(), respuesta.length());
+
+            ssize_t bytes_enviados = send(cliente_fd, respuesta.c_str(), respuesta.length(), 0);
+
             if (bytes_enviados < 0) {
                 error("Error al escribir en el socket");
             }
@@ -53,6 +55,10 @@ void manejar_cliente(int cliente_fd) {
     std::cout << "Conexión cerrada." << std::endl;
 }
 
+
+
+
+
 // Función para aceptar conexiones y crear hilos para cada cliente
 void aceptar_conexiones(int servidor_fd) {
     while (true) {
@@ -67,6 +73,8 @@ void aceptar_conexiones(int servidor_fd) {
         hilo_cliente.detach(); // El hilo se maneja por separado
     }
 }
+
+
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -114,7 +122,7 @@ int main(int argc, char *argv[]) {
     if (listen(servidor_fd, 1) < 0) {
         error("Error en el listen");
     }
-    std::cout << "Esperando conexión en el puerto " << puerto << "..." << std::endl;
+    fprintf(stdout, "Escuchando en el puerto %d...\n", puerto);
 
     if (aceptar_varios == "yes") {
         // Aceptar múltiples conexiones en un hilo
